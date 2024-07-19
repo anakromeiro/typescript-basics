30e8395baf231907225769f07ba08c54e6cf5a13
Karol <anakromeiro@gmail.com>
2024-07-19 15:15:51 +0100

    Create interface TODO

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -1,16 +1,23 @@
 import axios from "axios";
 
-const url = 'http://jsonplaceholder.typicode.com/todos/1'
+const url = "http://jsonplaceholder.typicode.com/todos/1";
+
+// Interface describe the structure of an object
+interface TODO {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 // This operation is async. It returns a promise
-axios.get(url).then(response => {
-    const todo = response.data;
-    
-    const id = todo.id;
-    const title = todo.title;
-    const isCompleted = todo.completed?'Done':'In Progress';
+axios.get(url).then((response) => {
+  const todo = response.data as TODO;
 
-    console.log(`
-        TODO ${id} - ${title} is ${isCompleted}
+  const id = todo.id;
+  const title = todo.title;
+  const isCompleted = todo.completed ? "Done" : "In Progress";
+
+  console.log(`
+        Task ${id} - ${title} is ${isCompleted}
         `);
 });
